7f98ac4a3d7459013a073a014b8a4541a865b109
Primus444 <pankaj85279@gmail.com>
2022-01-19 20:23:12 +0530

    add tabs function added

--- link_collector_chrome_Extention/script.js
+++ link_collector_chrome_Extention/script.js
@@ -1,61 +1,59 @@
 let welcomeMessage=document.getElementById("welcome");
 
-let username="";
- 
-let addcaught= [];
-localStorage.setItem("Links",JSON.stringify(addcaught));
-addcaught=JSON.parse(localStorage.getItem("links"));
-let inputval="";
+let addcaught=[];
+let local=[];
 let inputbt=document.getElementById("inputbt");
+let inputtbbt=document.getElementById("inputtbbt");
 let ulist=document.getElementById("ulist");
+let inputval="";
 let i=0;
-let j=0;
+let j=1;
+local=JSON.parse(localStorage.getItem("Links"));//input from local storage
+addcaught=addcaught.concat(local);//concatinating in run time variable.
+console.log(addcaught);
+updatelist();
 
-inputbt.addEventListener("click",inputvalue)
-
-
-function inputvalue(){
-    inputval=document.getElementById("inputval")
+inputbt.addEventListener("click",function inputvalue(){
+    console.log(localStorage.getItem("Links"));
+    inputval=document.getElementById("inputval");
     addcaught.push(inputval.value);
     inputval.value="";
+    updatelist();  
+
+})
+
+inputtbbt.addEventListener("click", getlink());
+
+function getlink(){
+   chrome.tabs.query({active: true , currentWindow: true}, function (tabs){
+    addcaught.push(tabs[0].url);
+    });
     updatelist();
 }
-inputtbbt.addEventListener("click",function(){
-    chrome.tabs.query({active: true , currentWindow:true },function (tabs){
-           console.log(tabs[0]);
-    });
-});
 
 function updatelist(){
-    if(j=0 && addcaught!=[])
+    localStorage.setItem("Links",JSON.stringify(addcaught));
+    if(j==1)
     {
-        displaylist();
+       for(i=0;i<addcaught.length;i++)
+        { 
+            j++;
+            const li=document.createElement("li");//<li></li>--created
+            const a=document.createElement('a');
+            a.setAttribute("href",addcaught[i]);
+            a.setAttribute("target","_blank");
+            a.textContent=addcaught[i];//<li>last element of array <li>--added
+            li.append(a);
+            ulist.append(li);//
+        }
     }
     else{
         const li=document.createElement("li");//<li></li>--created
-        const a=document.createElement('a');
-        a.setAttribute("href",addcaught[i]);
-        a.setAttribute("target","_blank");
-        console.log(a);
-        a.textContent=addcaught[i++];//<li>last element of array <li>--added
-        li.append(a);
-        ulist.append(li);//
+            const a=document.createElement('a');
+            a.setAttribute("href",addcaught[j++]);
+            a.setAttribute("target","_blank");
+            a.textContent=addcaught[i];//<li>last element of array <li>--added
+            li.append(a);
+            ulist.append(li);//
     }
 }
-
-function displaylist(){
-    for(i=0;i<addcaught.length;i++){
-
-    const li=document.createElement("li");//<li></li>--created
-    const a=document.createElement('a');
-    a.setAttribute("href",addcaught[i]);
-    a.setAttribute("target","_blank");
-    console.log(a);
-    a.textContent=addcaught[i++];//<li>last element of array <li>--added
-    li.append(a);
-    ulist.append(li);//
-    }
-}
-
-
-
